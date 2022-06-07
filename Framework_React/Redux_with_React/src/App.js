5d9349d63608ae91922934b1ab20827a896a8427
Georgiy <valyaev2002@bk.ru>
2022-06-07 17:53:34 +0300

    App frontend & backend

---
+++ Framework_React/Redux_with_React/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Likes from './Likes';
+import Title from './Title';
+import Comments from './Comments';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrap">
+        <div className="card">
+          <div className="card-image">
+            <img src="./messi.png" alt="surfing"/>
+            <Title />
+            <Likes />
+          </div>
+          <Comments />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
